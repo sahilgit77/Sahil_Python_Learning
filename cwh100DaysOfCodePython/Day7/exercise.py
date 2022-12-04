abec3c16f363aceeec6341307d1fcb8c28bc1f77
sahil.khatri <sahil.khatri@privacera.com>
2022-12-04 21:29:30 +0530

    Adding code for day-7, exercise

---
+++ cwh100DaysOfCodePython/Day7/exercise.py
@@ -0,0 +1,10 @@
+num1 = 5
+num2 = 2
+
+print("Addition = ", num1+num2 )
+print("Subtraction = ", num1-num2 )
+print("Multiplication = ", num1*num2 )
+print("Division = ", num1/num2 )
+print("Floor Division = ", num1//num2 )
+print("Modulo = ", num1%num2 )
+print("Exponential = ", num1**num2 )
